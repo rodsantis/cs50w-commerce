8fb7b0dc8677d5b7982aee0ab636808a2e85068c
Rodrigo De Santis <Rodrigo@Rodrigos-MBP.homenet.telecomitalia.it>
2024-05-21 16:02:32 +0200

    Showing Listings in the index page

--- auctions/models.py
+++ auctions/models.py
@@ -14,13 +14,23 @@ class Listing(models.Model):
     description = models.CharField(max_length=1000)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     # date = models.DateTimeField()
+    # TODO add the image entry also
+
+    def __str__(self):
+        return f"{self.title}: {self.description}, ${self.price}"
 
 
 class Comment(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comment")
     comments = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return f"Listing: {self.listing}, Comment: {self.comments}"
+
 
 class Bid(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bid")
     bids = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Listing: {self.listing}, Bids: {self.bids}"

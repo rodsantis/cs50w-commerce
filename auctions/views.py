a338bc7f4ed0047a100b9cb1aeb883333c716a23
Rodrigo De Santis <Rodrigo@Rodrigos-MBP.homenet.telecomitalia.it>
2024-05-22 06:16:21 +0200

    Implemented the create listing page in its basic form. The insertion to the data base for the new Listing is also working and being displayed in the home page

--- auctions/views.py
+++ auctions/views.py
@@ -1,10 +1,18 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
 from .models import User, Listing, Comment, Bid
+
+
+class NewListing(forms.Form):
+    title = forms.CharField(label='Title')
+    description = forms.CharField(label='Description', widget=forms.Textarea(attrs={"style": "height:300px"}))
+    price = forms.DecimalField(min_value=1.00, max_digits=10, decimal_places=2)
 
 
 def index(request):
@@ -63,3 +71,23 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+@login_required
+def create_listing(request, *args, **kwargs):
+    if request.method == "POST":
+        form = NewListing(request.POST)
+        if form.is_valid():
+            form_title = form.cleaned_data["title"]
+            form_description = form.cleaned_data["description"]
+            form_initial_bid = form.cleaned_data["price"]
+            new = Listing(title=form_title, description=form_description, price=form_initial_bid)
+            new.save()
+        else:
+            return render(request, "auctions/createlisting.html", {
+                "form": form
+            })
+
+    return render(request, "auctions/createlisting.html", {
+        "form": NewListing()
+    })
